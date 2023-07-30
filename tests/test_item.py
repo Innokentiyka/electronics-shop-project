5f1c4bc9b1c03f2324b031088d7025a47efcc24e
PC <kerik006@mail.ru>
2023-07-30 21:20:39 +0400

    homework 2

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,9 +1,17 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 import pytest
 from src.item import Item
+
+
 class TestCalculateTotalPrice:
-    def test_calculate_total_price(self):
+    def test_calculate_total_price(self, ):
         obj = Item("Ноут", 10, 5)
         result = obj.calculate_total_price()
         assert result == 50
 
+
+Item.instantiate_from_csv()
+assert len(Item.all) == 5
+assert Item.string_to_number('5') == 5
+assert Item.string_to_number('5.0') == 5.0
+assert Item.string_to_number('5.5') == 5.5
